28b27d4838286bd29d63cbc71ce1f0194906fb1b
Michael Sproul <michael@sigmaprime.io>
2021-10-05 21:40:01 +1100

    Accept list in API server

--- api_server.py
+++ api_server.py
@@ -14,29 +14,38 @@ class Classifier:
 
     def on_post(self, req, resp):
         try:
-            block_reward = json.load(req.bounded_stream)
+            block_rewards = json.load(req.bounded_stream)
         except json.decoder.JSONDecodeError as e:
             resp.text = json.dumps({"error": f"invalid JSON: {e}"})
             resp.code = falcon.HTTP_400
             return
 
-        # Check required fields
-        if ("block_root" not in block_reward or
-            "attestation_rewards" not in block_reward or
-            "per_attestation_rewards" not in block_reward["attestation_rewards"]):
-           resp.text = json.dumps({"error": "input JSON is not a single block reward"})
-           resp.code = falcon.HTTP_400
-           return
+        if type(block_rewards) != list:
+            resp.text = json.dumps({"error": "/classify accepts a list of block rewards"})
+            resp.code = falcon.HTTP_400
+            return
 
-        best_guess_single, best_guess_multi, probability_map = self.classifier.classify(block_reward)
+        results = []
 
-        result = {
-            "block_root": block_reward["block_root"],
-            "best_guess_single": best_guess_single,
-            "best_guess_multi": best_guess_multi,
-            "probability_map": probability_map,
-        }
-        resp.text = json.dumps(result, ensure_ascii=False)
+        for block_reward in block_rewards:
+            # Check required fields
+            if ("block_root" not in block_reward or
+                "attestation_rewards" not in block_reward or
+                "per_attestation_rewards" not in block_reward["attestation_rewards"]):
+               resp.text = json.dumps({"error": "input JSON is not a block reward"})
+               resp.code = falcon.HTTP_400
+               return
+
+            best_guess_single, best_guess_multi, probability_map = self.classifier.classify(block_reward)
+
+            results.append({
+                "block_root": block_reward["block_root"],
+                "best_guess_single": best_guess_single,
+                "best_guess_multi": best_guess_multi,
+                "probability_map": probability_map,
+            })
+
+        resp.text = json.dumps(results, ensure_ascii=False)
 
 app = application = falcon.App()
 
