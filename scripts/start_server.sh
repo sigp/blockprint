8c1dcfca06e065de1a6a830a8993c09b7a08e082
Michael Sproul <michael@sigmaprime.io>
2023-10-13 10:47:18 +1100

    Bind Docker interface for API server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,6 @@
 
 cd ..
 source ./venv/bin/activate
-gunicorn --timeout 1800 api_server:app
+# Bind on localhost and the Docker network interface.
+# This makes the server easily accessible to Docker containers without using --network=host.
+gunicorn --timeout 1800 --bind 127.0.0.1 --bind 172.17.0.1 api_server:app
