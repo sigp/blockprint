2931518592443f736f5e94dd74c03387a3e61ccf
Michael Sproul <michael@sigmaprime.io>
2024-01-25 11:43:26 +1100

    Fmt

--- tests/test_explode_gaps.py
+++ tests/test_explode_gaps.py
@@ -1,5 +1,6 @@
 from background_tasks import explode_gap
 
+
 def check_gaps(start_slot, end_slot, sprp):
     result = explode_gap(start_slot, end_slot, sprp=sprp)
 
@@ -7,12 +8,13 @@ def check_gaps(start_slot, end_slot, sprp):
 
     prev_start = None
     prev_end = None
-    for (start, end) in result:
+    for start, end in result:
         assert start < end
         assert start == start_slot or (start == prev_end + 1 and start % sprp == 1)
         assert end == end_slot or (end < end_slot and end % sprp == 0)
         prev_start = start
         prev_end = end
+
 
 def test_explode_large_gap():
     start_slot = 14273
@@ -20,11 +22,13 @@ def test_explode_large_gap():
     sprp = 2048
     check_gaps(start_slot, end_slot, sprp)
 
+
 def test_explode_small_gap_unaligned():
     start_slot = 1
     end_slot = 10
     sprp = 2048
     check_gaps(start_slot, end_slot, sprp)
+
 
 def test_explode_small_gap_aligned():
     start_slot = 1
